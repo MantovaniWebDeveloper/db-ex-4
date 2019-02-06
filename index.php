b36318b5aba9bd4b062765c528da05e179f05b0c
MantovaniWebDeveloper <ratedsuperstar84@gmail.com>
2019-02-06 14:24:38 +0100

    stampati in una tabella html tutti i dati della query

--- index.php
+++ index.php
@@ -3,16 +3,40 @@
   <head>
     <meta charset="utf-8">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/custom.css">
     <title>db-ex-4 by Dario Mantovani</title>
   </head>
   <body>
     <?php
     ini_set('display_errors', 1);
-                ini_set('display_startup_errors', 1);
-                error_reporting(E_ALL | E_STRICT);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL | E_STRICT);
     include("prenotazione.php");
-
     ?>
+    <div class="container">
+      <h2 class="text-center">Prenotazioni</h2>
+      <table class="table">
+        <thead class="thead-dark">
+          <tr>
+            <th class="text-center" scope="col">iD</th>
+            <th class="text-center" scope="col">prenotazione_id</th>
+            <th class="text-center" scope="col">ospite_id</th>
+          </tr>
+        </thead>
+        <tbody>
+          <?php foreach ($prenotazioni as  $prenotazione): ?>
+          <tr>
+            <td class="text-center"><?php echo $prenotazione["id"]; ?></td>
+            <td class="text-center"><?php echo $prenotazione["prenotazione_id"]; ?></td>
+            <td class="text-center"><?php echo $prenotazione["ospite_id"]; ?></td>
+          </tr>
+          <?php endforeach; ?>
+
+        </tbody>
+      </table>
+
+    </div>
+
 
   </body>
 </html>
--- prenotazione.php
+++ prenotazione.php
@@ -5,17 +5,19 @@
           from prenotazioni_has_ospiti";
 
   $resultPrenotazione = $conn->query($sql);
+  $prenotazioni = [];
 
   if ($resultPrenotazione->num_rows >0) {
 
     while ($row = $resultPrenotazione->fetch_array()) {
-      var_dump($row); die();
+      //var_dump($row); die();
+      $prenotazioni[] = $row;
     }
   }
   else {
     echo "errore";
   }
 
-
+  //var_dump($prenotazioni);
 
  ?>
